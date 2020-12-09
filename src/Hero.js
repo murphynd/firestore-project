1c5d9fdc1c7a2c5fa736d60c2023a29c4834192f
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-12-09 14:54:28 -0800

    adds tone, three and p5

--- src/Hero.js
+++ src/Hero.js
@@ -1,4 +1,12 @@
 import React from "react";
+import * as Tone from "tone";
+
+const synth = new Tone.MembraneSynth().toMaster();
+
+function playSynth() {
+  synth.triggerAttackRelease("C2", "8n");
+}
+
 const Hero = ({ handleLogout }) => {
   return (
     <section className="hero">
@@ -6,6 +14,9 @@ const Hero = ({ handleLogout }) => {
         <h2>Welcome</h2>
         <button onClick={handleLogout}>Logout</button>
       </nav>
+      <button id="button" onClick={playSynth}>
+        start
+      </button>
     </section>
   );
 };
